17abad4b28feabd8426705b29e5552bbb1f0bd55
CaioDam <161773985+CaioDam@users.noreply.github.com>
2024-11-12 01:30:13 -0300

    Add files via upload

---
+++ ProjectDarkestDungeon/menu.js
@@ -0,0 +1,5 @@
+console.log('teste')
+window.addEventListener("scroll", function() {
+    let header = document.querySelector('#header')
+    header.classList.toggle('rolagem', window.scrollY > 200)
+})
